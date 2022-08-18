384a87a64442087e9522373c98f2d53d5d406d86
MarErm27 <v27i72@gmail.com>
2022-08-18 03:29:10 +0300

    Fix test_data.sql

--- src/main/sql/test_data.sql
+++ src/main/sql/test_data.sql
@@ -3,12 +3,12 @@ CREATE TABLE person
     ID          SERIAL PRIMARY KEY,
     name        VARCHAR(255),
     age         INT,
-    insuranceId INT,
+    insurance_id INT,
     is_insured   boolean   DEFAULT false,
     created_at   timestamp DEFAULT now()
 );
 
-INSERT INTO person(name, age, isInsured)
+INSERT INTO person(name, age, is_insured)
 VALUES ('Vlad Dracul', 321, false);
-INSERT INTO person(name, age, insuranceId, isInsured)
+INSERT INTO person(name, age, insurance_id, is_insured)
 VALUES ('Joe Bloggs', 20, 1234567890, true);
